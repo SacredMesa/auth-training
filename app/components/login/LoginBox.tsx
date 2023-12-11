3e1b3834d28a5d7f76778cd4a6c6d84532b6eaca
Asyraaf <ahmadasyraaf@gmail.com>
2023-12-11 12:27:42 +0800

    :sparkles: add onSubmit handler

--- app/components/login/LoginBox.tsx
+++ app/components/login/LoginBox.tsx
@@ -1,4 +1,19 @@
+'use client'
+
+import React from 'react'
+import { signIn } from 'next-auth/react'
+
+interface FormFields {
+  username: HTMLInputElement;
+  password: HTMLInputElement;
+}
+
 export default function LoginBox() {
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = e.currentTarget.elements as typeof e.currentTarget.elements & FormFields;
+  }
+
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -7,19 +22,19 @@ export default function LoginBox() {
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               Sign in to your account
             </h1>
-            <form className="space-y-4 md:space-y-6 mt-6" action="#">
+            <form className="space-y-4 md:space-y-6 mt-6" onSubmit={onSubmit}>
               <div>
                 <label
-                  htmlFor="email"
+                  htmlFor="username"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                  Your email
+                  Your username
                 </label>
                 <input
-                  type="email"
-                  name="email"
-                  id="email"
+                  type="text"
+                  name="username"
+                  id="username"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="name@company.com"
+                  placeholder="Username"
                   required
                 />
               </div>
@@ -39,22 +54,6 @@ export default function LoginBox() {
                 />
               </div>
               <div className="flex items-center justify-between">
-                <div className="flex items-start">
-                  <div className="flex items-center h-5">
-                    <input
-                      id="remember"
-                      aria-describedby="remember"
-                      type="checkbox"
-                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                      required
-                    />
-                  </div>
-                  <div className="ml-3 text-sm">
-                    <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">
-                      Remember me
-                    </label>
-                  </div>
-                </div>
                 <a
                   href="#"
                   className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">
@@ -77,8 +76,9 @@ export default function LoginBox() {
             </form>
             <div className="inline-flex items-center justify-center w-full">
               <hr className="w-64 h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
-              <span
-                className="absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-1/2 dark:text-white dark:bg-gray-800">or</span>
+              <span className="absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-1/2 dark:text-white dark:bg-gray-800">
+                or
+              </span>
             </div>
             <div className="flex items-center justify-center w-full">
               <button
