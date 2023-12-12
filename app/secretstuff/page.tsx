40170a1d8ed877e67c37a2df8df7a804045d5cd8
Asyraaf <ahmadasyraaf@gmail.com>
2023-12-12 12:54:59 +0800

    :lipstick: improve secret ui and add unauthorised route

--- app/secretstuff/page.tsx
+++ app/secretstuff/page.tsx
@@ -1,7 +1,11 @@
-export default function SecretStuffPage() {
+import Image from 'next/image'
+
+export default async function SecretStuffPage() {
   return (
     <>
-      secret stuff
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <Image src="/vault.jpg" alt="me" width="300" height="300" />
+      </div>
     </>
   )
 }
